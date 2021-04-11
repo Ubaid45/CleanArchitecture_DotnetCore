b34ce9abb605233a5e5aea9ac998d46f527fe45e
Ubaid Rana <ubaidrana45@gmail.com>
2021-04-11 16:41:08 +0200

    Added Persistence

---
+++ GlobalTicket.TicketManagement.Application/Contracts/Persistence/IEventRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using GlobalTicket.TicketManagement.Domain.Entities;
+
+namespace GlobalTicket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface IEventRepository : IAsyncRepository<Event>
+    {
+        Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate);
+    }
+}
---
+++ GlobalTicket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GlobalTicket.TicketManagement.Domain.Entities;
+
+namespace GlobalTicket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface IOrderRepository: IAsyncRepository<Order>
+    {
+        Task<List<Order>> GetPagedOrdersForMonth(DateTime date, int page, int size);
+        Task<int> GetTotalCountOfOrdersForMonth(DateTime date);
+    }
+}
